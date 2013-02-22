a2ffeaf08dfa4ea19d77aac0bf9d95020f798971
Tomasz Długosz <tomek3d@gmail.com>
2013-02-22 21:38:48 +0100

    sort list of recipes

--- readme_updater.py
+++ readme_updater.py
@@ -1,20 +1,26 @@
 #!/usr/bin/env  python
 __license__   = 'GPL v3'
-__copyright__ = '2012, Tomasz Dlugosz <tomek3d@gmail.com>'
+__copyright__ = '2013, Tomasz Dlugosz <tomek3d@gmail.com>'
 
 import os
 
+www_recipes=[]
 tv_stations=[]
 for recipe in os.listdir('.'):
     if recipe.endswith('.recipe'):
         if not recipe.startswith('tv_'):
-            for line in open(recipe):
-                if 'description' in line:
-                   print '*',recipe,'-',line.split('\'')[1]
-                   break
+            www_recipes += [recipe]
         else:
-             tv_stations += [recipe]
+            tv_stations += [recipe]
 
+www_recipes.sort()
+for recipe in www_recipes:
+    for line in open(recipe):
+        if 'description' in line:
+           print '*',recipe,'-',line.split('\'')[1]
+           break
+print '### Program stacji telewizyjnych'
+tv_stations.sort()
 for recipe in tv_stations:
     print '*',recipe
 
